998bca64252b371ebd2800642abcca81ba4e9577
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-12-23 18:29:29 +0530

    initial commit

---
+++ appwrite-megablog/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+    <div className="text-4xl font-bold text-center">
+      Mega Blog Project
+      {console.log(import.meta.env.VITE_APPWRITE_URL)}
+    </div>
+  )
+}
+
+export default App
